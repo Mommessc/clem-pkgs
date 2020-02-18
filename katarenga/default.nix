7dd0c94cfaf21571c6ee3301e913c050099342c0
Mommessc <clement.mommessin@inria.fr>
2020-02-18 11:27:43 +0100

    Update katarenga

--- katarenga/default.nix
+++ katarenga/default.nix
@@ -1,23 +1,21 @@
 { stdenv
 , cmake
 , GLTK, glfw3
-, cppzmq, zeromq, docopt_cpp
+, zmqpp, docopt_cpp
 }:
 
 stdenv.mkDerivation rec {
 	name = "Katarenga-${version}";
 	version = "1.0.0-alpha";
 
-	# src = fetchGit {
-	# 	ref = "master";
-	# 	url = "https://github.com/Henry38/Katarenga.git";
-	# };
-	src = ~/Documents/Git/Katarenga;
+	src = fetchGit {
+		ref = "master";
+		url = "https://github.com/Henry38/Katarenga.git";
+	};
 
 	buildInputs = [
 		cmake
-		cppzmq
-		zeromq
+		zmqpp
 		docopt_cpp
 		GLTK
 	];
